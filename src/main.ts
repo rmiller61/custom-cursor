38d17d0c04d88ec93ea6abc6bd8112849ad4d077
rmiller61 <rmiller61@users.noreply.github.com>
2022-11-11 14:11:28 -0500

    3 files modified

--- src/main.ts
+++ src/main.ts
@@ -2,21 +2,39 @@ import './style.css';
 import { gsap } from 'gsap';
 
 // set the starting position of the cursor outside of the screen
-let clientX = -100;
-let clientY = -100;
+let clientX = 0;
+let clientY = 0;
 
 const wrapper = document.getElementById('cursor--wrapper');
 const video = document.getElementById('cursor--video') as HTMLVideoElement;
 //const cursor = document.getElementById('cursor');
 
 let isPlaying = true;
+let isTouch = false;
+
+window.addEventListener(
+  'touchstart',
+  function onFirstTouch() {
+    // we could use a class
+    isTouch = true;
+
+    // we only need to know once that a human touched the screen, so we can stop listening now
+    window.removeEventListener('touchstart', onFirstTouch, false);
+  },
+  false
+);
 
 const initCursor = () => {
   // add listener to track the current mouse position
   if (wrapper) {
+    const { x, y, height, width } = wrapper.getBoundingClientRect();
+    clientX = width / 2 - 50;
+    clientY = height / 2 - 50;
     wrapper.addEventListener('mousemove', (e) => {
-      clientX = e.clientX;
-      clientY = e.clientY;
+      if (!isTouch) {
+        clientX = e.clientX - x;
+        clientY = e.clientY - y;
+      }
     });
     wrapper.addEventListener('click', () => {
       if (isPlaying) {
@@ -32,6 +50,10 @@ const initCursor = () => {
   // transform the innerCursor to the current mouse position
   // use requestAnimationFrame() for smooth performance
   const render = () => {
+    gsap.to('.cursor', {
+      ease: 'linear',
+      opacity: 1,
+    });
     gsap.to('.cursor--icon', {
       ease: 'linear',
       x: clientX,
